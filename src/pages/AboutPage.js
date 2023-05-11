5ff58d3a4d5ade5d11ffc858b664c4c84e6f673b
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-05-11 08:01:47 +0200

    Create about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,22 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+    <main>
+      <PageHero title="about"/>
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, velit? Sapiente expedita similique consectetur fugit distinctio, tempora enim, incidunt illum quibusdam, nihil earum! Dignissimos, consequuntur aliquam? Nemo incidunt necessitatibus ipsum?</p>
+          </div>
+        </article>
+      </Wrapper>
+    </main>
+  )
 }
 
 const Wrapper = styled.section`
