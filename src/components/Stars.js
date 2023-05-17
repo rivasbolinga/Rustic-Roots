f00d9ba76e3ed1ee2fbda0cfb0853cf48f054d91
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-05-17 18:17:57 +0200

    Fix bug

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -5,12 +5,54 @@ const Stars = ({stars, reviews}) => {
   console.log(stars, reviews)
   return (
     <Wrapper>
-      <div className='stars'>
+      <div className="stars">
         <span>
-          {stars >=1? <BsStarFill /> : stars >= 0.5 ? <BsStarHalf /> : <BsStar />}
+          {stars >= 1 ? (
+            <BsStarFill />
+          ) : stars >= 0.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        <span>
+          {stars >= 2 ? (
+            <BsStarFill />
+          ) : stars >= 1.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        <span>
+          {stars >= 3 ? (
+            <BsStarFill />
+          ) : stars >= 2.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        <span>
+          {stars >= 4 ? (
+            <BsStarFill />
+          ) : stars >= 3.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        <span>
+          {stars === 5 ? (
+            <BsStarFill />
+          ) : stars >= 4.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
         </span>
       </div>
-      <p className='reviews'>({reviews} customer reviews)</p>
+      <p className="reviews">({reviews} customer reviews)</p>
     </Wrapper>
   )
 }
