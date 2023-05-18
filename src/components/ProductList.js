a29744329c628861c536b18504c934048f01092e
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-05-18 16:56:11 +0200

    Set up Product list with 3 if statement and rid View as default

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,16 +2,21 @@ import React from 'react'
 import { useFilterContext } from '../context/filter_context'
 import GridView from './GridView'
 import ListView from './ListView'
-
 const ProductList = () => {
-  const {filtered_products:products} = useFilterContext();
+  const { filtered_products: products, grid_view } = useFilterContext()
 
-  return (
-    <GridView products={products}>
-    </GridView>
-    <ListView products={products}>
-    </ListView>
-  )
+  if (products.length < 1) {
+    return (
+      <h5 style={{ textTransform: 'none' }}>
+        Sorry, no products matched your search.
+      </h5>
+    )
+  }
+
+  if (grid_view === false) {
+    return <ListView products={products} />
+  }
+  return <GridView products={products} />
 }
 
 export default ProductList
