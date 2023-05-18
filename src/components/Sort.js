ac37420d334343eba1dd94cf8f92a2fd79ecc588
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-05-18 18:06:17 +0200

    Create skeleton for sort component

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -3,7 +3,28 @@ import { useFilterContext } from '../context/filter_context'
 import { BsFillGridFill, BsList } from 'react-icons/bs'
 import styled from 'styled-components'
 const Sort = () => {
-  return <h4>sort </h4>
+  const {filtered_products: products, grid_view} = useFilterContext();
+  return (
+    <Wrapper>
+      <div className="btn-container">
+        <button type="button" className={`${grid_view ? 'active' : null}`}>
+          <BsFillGridFill />
+        </button>
+        <button type="button" className={`${!grid_view ? 'active' : null}`}>
+          <BsList />
+        </button>
+      </div>
+      <p>{products.length}products found</p>
+      <hr />
+      <label htmlFor="sort">sort by</label>
+      <select name="sort" id="sort" className="sort-input">
+        <option value="price-lowest">price (lowest)</option>
+        <option value="price-highest">price (highest)</option>
+        <option value="name-a">price (a-z)</option>
+        <option value="name-z">price (z-a)</option>
+      </select>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
