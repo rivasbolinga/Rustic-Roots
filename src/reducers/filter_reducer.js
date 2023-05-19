8045b55bb59bd16a701aa0698ba4a35442655eac
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-05-19 19:53:01 +0200

    Clear filters functionality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -58,6 +58,8 @@ const filter_reducer = (state, action) => {
   } if(action.type === UPDATE_FILTERS) {
     const {name, value} = action.payload
     return {...state, filters:{...state.filters, [name]:value}}
+  } if (action.type === CLEAR_FILTERS) {
+    return {...state, filters: {...state.filters}}
   }
   return state
   throw new Error(`No Matching "${action.type}" - action type`)
