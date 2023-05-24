7e07bb109e0132e4df9aae13b43a5aa8c9f8f260
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-05-24 21:36:49 +0200

    Style About Page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,22 +1,54 @@
 import React from 'react'
 import styled from 'styled-components'
 import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import aboutImg from '../assets/hero-bcg.png'
 
 const AboutPage = () => {
   return (
     <main>
-      <PageHero title="about"/>
-      <Wrapper className='page section section-center'>
-        <img src={aboutImg} alt="nice desk" />
-        <article>
-          <div className='title'>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <div className="first-div">
+          <img src={aboutImg} alt="nice desk" />
+          <div className="title">
             <h2>our story</h2>
-            <div className='underline'>
-            </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, velit? Sapiente expedita similique consectetur fugit distinctio, tempora enim, incidunt illum quibusdam, nihil earum! Dignissimos, consequuntur aliquam? Nemo incidunt necessitatibus ipsum?</p>
+            <p>
+              In the charming French countryside, Rustic Root, an online
+              furniture shop, flourished for 20 years. Founded in 2003 by
+              Amélie, a furniture enthusiast, it curated rustic pieces that
+              exuded the essence of French elegance. Initially facing
+              challenges, Rustic Root's commitment to quality attracted
+              customers worldwide.
+              </p>
+              <p>
+                With a user-friendly website and detailed product descriptions,
+                it became renowned as a go-to destination for unique furniture.
+              </p>
           </div>
-        </article>
+        </div>
+        <div className='second-div'>
+          <img src={aboutImg} alt="nice desk" />
+          <p>
+                Expanding its range over the years, Rustic Root sourced from
+                local artisans, preserving craftsmanship traditions. By its 10th
+                anniversary, the brand had grown exponentially, supported by a
+                team of experts. To mark its 20th year, Rustic Root opened a
+                physical showroom, inviting customers to experience its curated
+                collection firsthand. The grand opening was a success, drawing
+                visitors from near and far.
+              </p>
+              <p>
+                The showroom, nestled in a quaint village, added depth to Rustic
+                Root's legacy, enhancing the bond with its customers. The brand
+                thrived amid changing trends, embracing digital platforms and
+                connecting through social media. Today, Rustic Root stands tall
+                as a symbol of excellence in online furniture retail. From a
+                dream to a revered brand, it leaves an indelible mark on the
+                world of furniture. With unwavering dedication to preserving
+                rustic elegance and French heritage, Rustic Root invites
+                customers into a world where timeless beauty reigns.
+              </p>
+        </div>
       </Wrapper>
     </main>
   )
@@ -38,6 +70,11 @@ const Wrapper = styled.section`
     margin: 0 auto;
     margin-top: 2rem;
     color: var(--clr-grey-5);
+    font-family: 'Playfair';
+  }
+  h2 {
+    font-size: 40px;
+    text-align: center;
   }
   .title {
     text-align: left;
@@ -45,7 +82,7 @@ const Wrapper = styled.section`
   .underline {
     margin-left: 0;
   }
-  @media (min-width: 992px) {
+  @media (min-width: 768px) {
     grid-template-columns: 1fr 1fr;
   }
 `
