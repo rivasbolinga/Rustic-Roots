6cb913bf58ad1d522eff9500c2c9aee44c1198dd
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-05-28 17:42:44 +0200

    Create Cart item

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -6,7 +6,6 @@ import { formatPrice } from '../utils/helpers'
 import { Link } from 'react-router-dom'
 const CartTotals = () => {
   const { total_amount, shipping_fee } = useCartContext()
-  const { myUser, loginWithRedirect } = useUserContext()
 
   return (
     <Wrapper>
@@ -23,7 +22,7 @@ const CartTotals = () => {
             order total :<span>{formatPrice(total_amount + shipping_fee)}</span>
           </h4>
         </article>
-        {myUser ? (
+        {/* {myUser ? (
           <Link to="/checkout" className="btn">
             proceed to checkout
           </Link>
@@ -31,7 +30,7 @@ const CartTotals = () => {
           <button onClick={loginWithRedirect} className="btn">
             login
           </button>
-        )}
+        )} */}
       </div>
     </Wrapper>
   )
