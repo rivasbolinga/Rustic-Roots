88a1a93ba6357ed32e41c19d506637c19125a1df
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-06-05 15:29:11 +0200

    Add Auth0 provider

--- src/index.js
+++ src/index.js
@@ -12,11 +12,19 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain="dev-635ounsp7aik3c5z.us.auth0.com"
+    clientId="1E2kD2igwFO8tPQYMpax8yPznwX6vvDW"
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>
 )
