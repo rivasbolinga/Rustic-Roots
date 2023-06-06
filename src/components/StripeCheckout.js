fda4feecfd0cadbb59b810dc7fae0e9db3572d7f
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-06-06 16:17:19 +0200

    Add cardStyle

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -12,14 +12,37 @@ import { useCartContext } from '../context/cart_context'
 import { formatPrice } from '../utils/helpers'
 import { useHistory } from 'react-router-dom'
 
+
+const promise =loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
 const CheckoutForm = () => {
+  const cardStyle = {
+    style: {
+      base: {
+        color: '#32325d',
+        fontFamily: 'Arial, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+          color: '#32325d',
+        },
+      },
+      invalid: {
+        color: '#fa755a',
+        iconColor: '#fa755a',
+      },
+    },
+  }
   return <h4>hello from Stripe Checkout </h4>
 }
+
 
 const StripeCheckout = () => {
   return (
     <Wrapper>
-      <CheckoutForm />
+      <Elements stripe={promise}>
+        <CheckoutForm />
+      </Elements>
     </Wrapper>
   )
 }
